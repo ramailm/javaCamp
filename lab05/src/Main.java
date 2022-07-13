925b568b5aa74c2027d40a196df29555c2e11530
Sumeyye Karagul <90051994+ramailm@users.noreply.github.com>
2022-07-13 22:59:04 +0300

    Update Main.java

--- lab05/src/Main.java
+++ lab05/src/Main.java
@@ -5,12 +5,11 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int rows,cols,numMines;
-        System.out.println("sorasıyla mayın tarlanızın satır,sütun ve mayınlı bölge adet sayınızı giriniz.. ");
         rows = sc.nextInt();
         cols = sc.nextInt();
         numMines = sc.nextInt();
         MyMinesweeper m = new MyMinesweeper(rows,cols,numMines);
-        //bundan sonrasında consol üzerinde oynanması için iyileştirilmeye gidilebilir..
+        //bundan sonrasi gelistirilebilir
         m.printClues(m.generateClues());
     }
 }
